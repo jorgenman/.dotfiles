f380dd6bd4573ac8175fb1ae3dfa9ae14609d095
Aaron Jorgensen <aaron.jorgensen@vivint.com>
2023-01-24 09:29:02 -0700

    Add paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,11 @@
-starship init fish | source
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
 
 alias ll="ls -al"
+
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.local/bin
+fish_add_path $HOME/scripts
+
+starship init fish | source
